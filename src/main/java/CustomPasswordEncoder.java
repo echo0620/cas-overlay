2e2d9be2f00ba234248d44f2b1f27e75bf882b36
echo0620 <ningchao2job@126.com>
2018-07-25 10:23:19 +0800

    Initial commit

---
+++ src/main/java/CustomPasswordEncoder.java
@@ -0,0 +1,18 @@
+public class CustomPasswordEncoder {
+
+    public static void main(String[] args){
+
+
+        int a =10;
+        System.out.println(a);
+        System.out.println("a = " + a);
+        System.out.println("args = [" + args + "]");
+
+
+    }
+
+
+    public void mthod1(){
+
+    }
+}
